16dbfba121641fcce7b32139e5647a877feffd61
raTMole <ratmole@gmail.com>
2017-08-23 11:40:03 +0300

    whitespace removed, re-enabled focus onCreate

--- src/main/java/com/kaytat/simpleprotocolplayer/MainActivity.java
+++ src/main/java/com/kaytat/simpleprotocolplayer/MainActivity.java
@@ -93,26 +93,20 @@ public class MainActivity extends Activity implements OnClickListener {
         mPlayButton.setOnClickListener(this);
         mStopButton.setOnClickListener(this);
 
-        //disable focus onCreate
-        mIPAddrText.setFocusableInTouchMode(false);
-        mAudioPortText.setFocusableInTouchMode(false);
-
-
-
         // Allow full list to be shown on first focus
         mIPAddrText.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                    mIPAddrText.showDropDown();
-               return false;
+                mIPAddrText.showDropDown();
+                return false;
             }
         });
-
         mIPAddrText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (hasFocus && mIPAddrText.getAdapter() != null)
                     mIPAddrText.showDropDown();
+
             }
         });
         mAudioPortText.setOnTouchListener(new View.OnTouchListener() {
